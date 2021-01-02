6174f68889c5d193e5f35e6a92cd039ab35fef6c
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-01-02 15:47:46 -0800

    Add files via upload

---
+++ Wave 5/Numpy Labs/abc_numpy.py
@@ -0,0 +1,26 @@
+#Create a numpy array of strings containing letters 'a' through 'j' (inclusive) of the alphabet. Then, use numpy array attributes to print the following information about this array:
+
+#dtype of array
+
+#shape of array
+#size of array
+
+#The code you submit in the code editor below will not be graded. Use the results from your code below, along with what you remember from the previous video, to complete the quiz below the code editor.
+
+
+
+import numpy as np
+
+# create numpy array of letters a-j
+
+print("Letter Array: ", letter_array)
+
+# get dtype of array
+
+
+# get shape of array
+
+
+# get size of array
+#%%
+# %%
